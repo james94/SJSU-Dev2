d48ea0e134b94e6f49573081f59c0f4c4f87b584
ubuntu <ubuntu@ubun2004.linuxvmimages.local>
2020-11-09 15:26:38 -0500

    one step closer

---
+++ demos/stm32f10x/pwm/source/main.cpp
@@ -0,0 +1,9 @@
+#include "L1_Peripheral/stm32f10x/gpio.hpp"
+#include "utility/log.hpp"
+#include "utility/time.hpp"
+
+int main()
+{
+
+  return 0;
+}
--- library/L1_Peripheral/stm32f10x/pwm.hpp
+++ library/L1_Peripheral/stm32f10x/pwm.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
+#include "L0_Platform/stm32f10x/stm32f10x.h"
+#include "L1_Peripheral/pwm.hpp"
 
 
-namespace sjsu
-{
-namespace stm32f10x
+namespace sjsu::stm32f10x
 {
 /// An implementation of the sjsu::Pwm peripheral for the stm32f10x family of
 /// microcontrollers.
@@ -13,7 +13,42 @@ namespace stm32f10x
 /// for all other PWM channels.
 class Pwm final : public sjsu::Pwm
 {
+public:
+    void ConfigureFrequency(units::frequency::hertz_t frequency) override
+    {
+        
+    }
 
-}
-}  // namespace stm32f10x
-}  // namespace sjsu
+    void SetDutyCycle(float duty_cycle) override
+    {
+
+    }
+
+    float GetDutyCycle() override
+    {
+
+    }
+
+    void ModuleInitialize()
+    {
+        // Auto Reload Register is not used
+        TIM1->ARR = 0;
+
+        // Setting duty cycle to be 75%
+        TIM1->CCR1 = 49152;
+
+        // Setting channel to output (reset value)
+        TIM1->CCMR1 &= ~(3 << 8);
+
+        // Setting output to PWM type 1
+        TIM1->CCMR1 &= ~(7 << 4);
+        TIM1->CCMR1 |= (6 << 4);
+
+        // Enable time
+        TIM1->CCER |= (1 << 0);
+
+        // Enable timer
+        TIM1->CR1 |= (1 << 0);
+    }
+};
+}  // namespace sjsu::stm32f10x
